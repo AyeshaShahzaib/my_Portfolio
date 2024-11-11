5aed1e4fb6a0b574a67d00723e36239cd8a00769
AyeshaShahzaib <ayeshashahzaib007@gmail.com>
2024-11-11 10:43:02 +0500

    latest

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,4 @@
 import Hero_Section from "./components/hero_section";
-import About from "./components/Aboutus";
-import ContactSection from "./components/Contact_me";
  export default function My_Portfolio(){
 return(
     <>
